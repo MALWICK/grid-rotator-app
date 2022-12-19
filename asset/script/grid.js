b226ce046c8fcf35dcff67e937d1d6105ae90d0a
MALWICK <batahumphrey66@gmail.com>
2022-12-19 14:32:50 +0100

    correcting linter errors

--- asset/script/grid.js
+++ asset/script/grid.js
@@ -1,16 +1,15 @@
-const btn1 = document.getElementById('button1')
-const btn2 = document.getElementById('button2')
-const btn3 = document.getElementById('button3')
-const btn4 = document.getElementById('button4')
-const btn6 = document.getElementById('button6')
-const btn7 = document.getElementById('button7')
-const btn8 = document.getElementById('button8')
-const btn9 = document.getElementById('button9')
+const list = [1, 4, 7, 8, 9, 6, 3, 2]
+
+const btn1 = document.getElementById('1')
+const btn2 = document.getElementById('2')
+const btn3 = document.getElementById('3')
+const btn4 = document.getElementById('4')
+const btn6 = document.getElementById('6')
+const btn7 = document.getElementById('7')
+const btn8 = document.getElementById('8')
+const btn9 = document.getElementById('9')
 const rotateNumber = document.querySelectorAll('.numchanger')
 const btn5 = document.querySelector('.numberchanger')
-console.log(btn5)
-
-const list = [1, 2, 3, 6, 9, 8, 7, 4]
 
 btn5.addEventListener('click', function () {
   reverseNumber()
@@ -19,10 +18,10 @@ btn5.addEventListener('click', function () {
 
 function reverseNumber () {
   const index1 = list.pop()
-  list.unshift('index1')
+  list.unshift(index1)
 }
 
-for (i of rotateNumber) {
+for (const i of rotateNumber) {
   i.addEventListener('click', function () {
     antiWise()
     rotate()
@@ -31,16 +30,16 @@ for (i of rotateNumber) {
 
 function antiWise () {
   const index1 = list.shift()
-  list.push('index1')
+  list.push(index1)
 }
 
 function rotate () {
   btn1.innerHTML = list[0]
-  btn2.innerHTML = list[1]
-  btn3.innerHTML = list[2]
-  btn4.innerHTML = list[7]
-  btn6.innerHTML = list[3]
-  btn7.innerHTML = list[6]
-  btn8.innerHTML = list[5]
+  btn2.innerHTML = list[7]
+  btn3.innerHTML = list[6]
+  btn4.innerHTML = list[1]
+  btn6.innerHTML = list[5]
+  btn7.innerHTML = list[2]
+  btn8.innerHTML = list[3]
   btn9.innerHTML = list[4]
 }
