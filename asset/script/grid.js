f02de20d1144c9ee45890e9284f45fbf7bd777c9
MALWICK <batahumphrey66@gmail.com>
2022-12-19 12:26:06 +0100

    correcting my linter errors

--- asset/script/grid.js
+++ asset/script/grid.js
@@ -1,29 +1,29 @@
-const btn1 = document.getElementById("button1");
-let btn2 = document.getElementById("button2");
-let btn3 = document.getElementById("button3");
-let btn4 = document.getElementById("button4");
-let btn6 = document.getElementById("button6");
-let btn7 = document.getElementById("button7");
-let btn8 = document.getElementById("button8");
-let btn9 = document.getElementById("button9");
-const rotateNumber = document.querySelectorAll(".numchanger");
-let btn5 = document.querySelector(".numberchanger");
+const btn1 = document.getElementById('button1');
+let btn2 = document.getElementById('button2');
+let btn3 = document.getElementById('button3');
+let btn4 = document.getElementById('button4');
+let btn6 = document.getElementById('button6');
+let btn7 = document.getElementById('button7');
+let btn8 = document.getElementById('button8');
+let btn9 = document.getElementById('button9');
+const rotateNumber = document.querySelectorAll('.numchanger');
+let btn5 = document.querySelector('.numberchanger');
 console.log(btn5);
 
 let list = [1, 2, 3, 6, 9, 8, 7, 4];
 
-btn5.addEventListener("click", function () {
+btn5.addEventListener('click', function () {
   reverseNumber();
   rotate();
 });
 
 function reverseNumber() {
   let index1 = list.pop();
-  list.unshift("index1");
+  list.unshift('index1');
 }
 
 for (i of rotateNumber) {
-  i.addEventListener("click", function () {
+  i.addEventListener('click', function () {
     antiWise();
     rotate();
   });
@@ -31,9 +31,8 @@ for (i of rotateNumber) {
 
 function antiWise() {
   let index1 = list.shift();
-  list.push(index1);
+  list.push('index1');
 }
-
 function rotate() {
   btn1.innerHTML = list[0];
   btn2.innerHTML = list[1];
